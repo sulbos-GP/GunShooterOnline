06fc1dec3a5a3d9eb1814415c98d3212f6d08143
Apeirogon99 <Apeirogon99@gmail.com>
2024-07-24 18:26:00 +0900

    Docker파일 수정

---
+++ GSO_WebServer/GameServerManager/Servicies/SessionBackgroundService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GameServerManager.Servicies
+{
+    public class SessionBackgroundService : BackgroundService
+    {
+        private PeriodicTimer? mTimer = null;
+        private const double mPeriodSecond = 1;                           //1초마다 실행
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            mTimer = new PeriodicTimer(TimeSpan.FromSeconds(mPeriodSecond));
+
+            while (await mTimer.WaitForNextTickAsync(stoppingToken))
+            {
+                stopwatch.Restart();
+
+                //DO SOMETHINGS
+
+                stopwatch.Stop();
+                TimeSpan elapsed = TimeSpan.FromSeconds(mPeriodSecond) - stopwatch.Elapsed;
+                if (elapsed.Microseconds <= 0)
+                {
+                    Console.WriteLine("Background over time: " + elapsed.TotalMilliseconds + " ms");
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+
+            base.Dispose();
+
+            if (mTimer != null)
+            {
+                mTimer.Dispose();
+            }
+
+        }
+    }
+}
