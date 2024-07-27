381d9cd6f1dce9ab9373173fff3135e9bf9b9604
Apeirogon99 <Apeirogon99@gmail.com>
2024-07-27 17:45:12 +0900

    매칭 UI 설정

--- GSO_Client/Assets/Gwanho/UI_Match.cs
+++ GSO_Client/Assets/Gwanho/UI_Match.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static AuthorizeResource;
+using static MatchmakerResource;
 
 public class UI_Match : MonoBehaviour
 {
@@ -10,21 +12,27 @@ public class UI_Match : MonoBehaviour
     private Button mMatchButton;
 
     [SerializeField]
-    private TextMeshProUGUI mMatchText;
+    private TextMeshProUGUI mMatchModeText;
 
-    private bool mIsJoin = false;
+    [SerializeField]
+    private TextMeshProUGUI mMatchTimeText;
+
+    private GameObject mLoadingScreen;
+
+    private bool mIsProcessMatch = false;
+    private bool mIsJoin = true;                //True=매칭참여, False=매칭취소
 
     private void Awake()
     {
-        mMatchButton.onClick.AddListener(OnMatchJoin);
-        mMatchText.text = "시작하기";
+        mMatchButton.onClick.AddListener(OnClickMatch);
+        mMatchModeText.text = "매칭 참여";
+        mMatchTimeText.transform.parent.gameObject.SetActive(false);
     }
 
     void Start()
     {
         //임시
         //나중에 로비에 입장할떄마다 연결 할 수 있도록
-
 
 
     }
@@ -36,18 +44,132 @@ public class UI_Match : MonoBehaviour
 
     }
 
-    private void OnMatchJoin()
+    /// <summary>
+    /// 매칭 버튼 클릭시
+    /// </summary>
+    private void OnClickMatch()
     {
-        mMatchText.text = "취소하기";
+        if(mIsProcessMatch)
+        {
+            SystemLogManager.Instance.LogMessage("매칭 작업이 진행중 입니다. 조금 뒤에 다시 시도해 주세요");
+            return;
+        }
 
-
-
+        if(mIsJoin)
+        {
+            OnMatchJoin();
+        }
+        else
+        {
+            OnMatchCancle();
+        }
 
     }
 
+    /// <summary>
+    /// 매칭 참여 요청
+    /// </summary>
+    private void OnMatchJoin()
+    {
+        SystemLogManager.Instance.LogMessage("매칭 참여 요청...");
+        mIsProcessMatch = true;
+
+        var packet = new MatchmakerJoinReq
+        {
+
+        };
+
+        MatchmakerService service = new MatchmakerService();
+        MatchJoinRequest request = service.mMatchmakerResource.GetMatchJoinRequest(packet);
+        request.ExecuteAsync(OnProcessMatchJoin);
+    }
+
+    /// <summary>
+    /// 매칭 참여 응답
+    /// </summary>
+    private void OnProcessMatchJoin(MatchmakerJoinRes response)
+    {
+        mIsProcessMatch = false;
+        if (response.error_code == 0)
+        {
+            SystemLogManager.Instance.LogMessage("매칭 참여 요청 성공");
+
+            mMatchModeText.text = "매칭 취소";
+            mIsJoin = false;
+
+            mMatchTimeText.transform.parent.gameObject.SetActive(true);
+            StartCoroutine(UpdateTimer());
+        }
+        else
+        {
+            SystemLogManager.Instance.LogMessage("매칭 참여 요청 실패");
+        }
+    }
+
+    /// <summary>
+    /// 매칭 취소 요청
+    /// </summary>
     private void OnMatchCancle()
     {
-        mMatchText.text = "시작하기";
+        SystemLogManager.Instance.LogMessage("매칭 취소 요청...");
+        mIsProcessMatch = true;
+
+        var packet = new MatchmakerCancleReq
+        {
+
+        };
+
+        MatchmakerService service = new MatchmakerService();
+        MatchCancleRequest request = service.mMatchmakerResource.GetMatchCancleRequest(packet);
+        request.ExecuteAsync(OnProcessMatchCancle);
+    }
+
+    /// <summary>
+    /// 매칭 취소 응답
+    /// </summary>
+    private void OnProcessMatchCancle(MatchmakerCancleRes response)
+    {
+        mIsProcessMatch = false;
+        if (response.error_code == 0)
+        {
+            SystemLogManager.Instance.LogMessage("매칭 취소 요청 성공");
+
+            mMatchModeText.text = "매칭 참여";
+            mIsJoin = true;
+
+            mMatchTimeText.transform.parent.gameObject.SetActive(false);
+            StopCoroutine(UpdateTimer());
+        }
+        else
+        {
+            SystemLogManager.Instance.LogMessage("매칭 취소 요청 실패");
+        }
+    }
+
+    /// <summary>
+    /// 대기시간 타이머 업데이트
+    /// </summary>
+    private IEnumerator UpdateTimer()
+    {
+        int elapsedTime = 0;
+        const float delay = 1.0f;
+
+        while (!mIsJoin)
+        {
+            elapsedTime += 1;
+            UpdateTimerDisplay(elapsedTime);
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+
+    private void UpdateTimerDisplay(int elapsedTime)
+    {
+        int seconds = elapsedTime;
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        mMatchTimeText.text = string.Format("{0:D2}:{1:D2}", minutes, seconds);
     }
 
 }
