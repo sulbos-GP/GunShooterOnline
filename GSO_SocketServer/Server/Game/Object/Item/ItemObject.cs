fc9da44f8c7356900d65a03d91a57017c8cc76e6
jish0902 <84966162+jish0902@users.noreply.github.com>
2024-07-31 15:49:01 +0900

    .
    .

---
+++ GSO_SocketServer/Server/Game/Object/Item/ItemObject.cs
@@ -0,0 +1,14 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Game
+{
+    internal class ItemObject
+    {
+         
+    }
+}
