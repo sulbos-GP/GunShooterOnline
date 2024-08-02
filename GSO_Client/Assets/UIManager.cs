4aad2239efc72a8d13ab170ff14b420d9fdfe855
unknown <gs2576@kangnam.ac.kr>
2024-08-02 15:12:18 +0900

    스텟 로직 변경 / 플레이어 스폰 로직 추가(Pos 제외) / Manager 추가

--- GSO_Client/Assets/UIManager.cs
+++ GSO_Client/Assets/UIManager.cs
@@ -7,10 +7,10 @@ public class UIManager : MonoBehaviour
 {
     public TMP_Text Health;
     //TO-DO : 로컬 플레이어 생길 시 제거
-    public Unit LocalUnit;
+    public Unit LocalUnit => UnitManager.Instance.CurrentPlayer;
 
     public TMP_Text AmmoText;
-    public Gun Gun;
+    public Gun Gun => LocalUnit.GetComponentInChildren<Gun>();
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +21,7 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         //HP
-         Health.text =  "HP : "+LocalUnit._health.ToString()+" %";
+         Health.text =  "HP : "+LocalUnit.unitStat.GetStat(eStat.Health).ToString()+" %";
         //GunAmmo
         AmmoText.text = Gun.getGunStat().name+" :: "+Gun._curAmmo.ToString();
     }
