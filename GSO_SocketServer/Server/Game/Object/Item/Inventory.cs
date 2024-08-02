6416795034dc1e84981c2301058f2b45a41501f6
201904042 <packtty18@naver.com>
2024-08-02 16:03:24 +0900

    .

--- GSO_SocketServer/Server/Game/Object/Item/Inventory.cs
+++ GSO_SocketServer/Server/Game/Object/Item/Inventory.cs
@@ -85,20 +85,24 @@ namespace Server.Game
                 restSize -= canInsertlist[random].Width * canInsertlist[random].Height;
             }
 
-
             return newData;
         }
 
 
-        /*
+        
         public void MoveItem(int id, int posX, int posY)
         {
             //아이템 가져오기
             ItemObject target = ObjectManager.Instance.Find<ItemObject>(id);
+
+            if(target == null)
+            {
+                return;
+            }
             //TODO : instantGrid[0].ItemPushCheck
 
 
-            // 지승현 -> 박성훈 
+            /* 지승현 -> 박성훈 
             instantGrid[0].ItemPushCheck(target, posX, posY);
 
             if (target != null)
@@ -110,21 +114,10 @@ namespace Server.Game
 
                     instantGrid[0].
                 }
-            }
+            }*/
 
-
-
-            
-
-             C_MoveItemHandler에서 패킷으로 받은 아이템 처리할때 아이템의 위치만 옮기는게 아니라 만약 해당 위치에 
-            이미 아이템이 있다면 ItemDataInfo.isItemConsumable이 true고 아이템코드가 서로 같은지 확인하고  
-            amount의 갯수를 늘리게 해야되  이게 내가 준 itemObject의 MergeItem이에
-
-             
-        }*/
-
-
-
-
+            target.OwnerGrid.DeleteItemFromSlot(target);
+            target.OwnerGrid.PushItemIntoSlot(target,posX,posY);
+        }
     }
 }
