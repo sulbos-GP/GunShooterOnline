ae1fc0d6e453eed5371d537d89ab3728ef6401a9
packtty18 <packtty18@naver.com>
2024-08-03 20:00:12 +0900

    Exit존 코루틴 부분 누락되어 추가 업데이트

--- GSO_Client/Assets/Scripts/Inventory/InteractableObj/ExitZone.cs
+++ GSO_Client/Assets/Scripts/Inventory/InteractableObj/ExitZone.cs
@@ -6,9 +6,8 @@ using UnityEngine;
 
 public class ExitZone : InteractableObject
 {
-    public InvenData invenData;
-    public GameObject gameEndUI;
-
+    // public GameObject gameEndUI;아직없음
+    public float ExitTime;  //나가는데 걸리는 시간
 
     private void Awake()
     {
@@ -31,15 +30,29 @@ public class ExitZone : InteractableObject
 
     public override void Interact()
     {
+        StartCoroutine(ExitCoroutine(ExitTime));
+        
+    }
+
+    public void InterruptExit()
+    {
+        StopCoroutine(ExitCoroutine(ExitTime));
+    }
+
+    public IEnumerator ExitCoroutine(float exitTime)
+    {
+        yield return new WaitForSeconds(exitTime);
+
         //인게임 종료 및 플레이어를 로비씬으로 이동.(인벤토리 보존)
-        //서버에 탈출한 플레이어의 ID, 탈출구 ID를 전송
+        //서버에 탈출한 플레이어의 ID
 
         //C_LeaveGame packet = new C_LeaveGame();
-        
+
         //Managers.Network.Send(packet);
         Debug.Log("C_LeaveGame");
 
         //게임씬을 로비로
         Managers.Scene.LoadScene(Define.Scene.Lobby);
+
     }
 }
