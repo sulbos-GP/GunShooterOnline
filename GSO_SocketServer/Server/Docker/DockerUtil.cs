67d949b8bd31cdf66fc6bbe58a1ec4a2758bb7e8
apeirogon99 <apeirogon99@gmail.com>
2024-08-06 22:37:19 +0900

    소켓 서버 도커 컨테이너 생성 테스트 확인 및 게임 서버 매니저와 웹 통신 확인

---
+++ GSO_SocketServer/Server/Docker/DockerUtil.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Docker
+{
+    public static class DockerUtil
+    {
+        public static string GetContainerId()
+        {
+            string[] cgroupLines = File.ReadAllLines("/proc/self/cgroup");
+            string cgroupLine = cgroupLines.FirstOrDefault(line => line.Contains("cpu:"));
+
+            if (cgroupLine != null)
+            {
+                return cgroupLine.Split('/').Last();
+            }
+
+            return string.Empty;
+        }
+
+        public static string GetHostIP()
+        {
+            string ip = Environment.GetEnvironmentVariable("HOST_IP");
+            if (ip == null)
+            {
+                return string.Empty;
+            }
+            return ip;
+        }
+
+        public static int GetHostPort()
+        {
+            string port = Environment.GetEnvironmentVariable("HOST_PORT");
+            if (port == null)
+            {
+                return 0;
+            }
+            return int.Parse(port);
+        }
+
+        public static int GetRegister()
+        {
+            return Convert.ToInt32(Environment.GetEnvironmentVariable("REGISTER"));
+        }
+
+        public static int GetBacklog()
+        {
+            return Convert.ToInt32(Environment.GetEnvironmentVariable("BACKLOG"));
+        }
+    }
+}
--- GSO_SocketServer/Server/Program.cs
+++ GSO_SocketServer/Server/Program.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Net;
-using System.Threading;
-using QuadTree;
-using Server.Game;
+using WebClientCore;
+using Server.Docker;
 
 using ServerCore;
+using Server.DTO;
+using System.Threading.Tasks;
 
 namespace Server
 {
+
 	class Program
 	{
-
-        Thread t;
-
-
 		public static ServerNetworkService mNetworkService = new ServerNetworkService();
+        public static WebClientService mWebClientService = new WebClientService();
 
         public static ushort ServerTickCount { get; internal set; } = 0;
         public static int ServerIntervalTick { get; internal set; } = 250;
@@ -32,15 +31,33 @@ namespace Server
         static void Main(string[] args)
 		{
 
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress iPAddress;
+            int port = 0;
+            string acceptKey = string.Empty;
+            int register = 0;
+            int backLog = 0;
 
-            //IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 7777);
+#if DOCKER
+            iPAddress   = IPAddress.Any;
+            port        = DockerUtil.GetHostPort();
+            acceptKey   = DockerUtil.GetContainerId();
+            register    = DockerUtil.GetRegister();
+            backLog     = DockerUtil.GetBacklog();
+#else
+
+            iPAddress = IPAddress.Loopback;
+            port = 7777;
+            acceptKey = "SomeConnectionKey";
+            register = 100;
+            backLog = 100;
+#endif
+
+            InitWebClientService();
 
             Func<Session> session = () => { return new ClientSession(); };
 
-            mNetworkService.Init(endPoint, session, "SomeConnectionKey", 100, 100);
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, port);
+            mNetworkService.Init(endPoint, session, acceptKey, register, backLog);
             
             BattleGameRoom room = new BattleGameRoom();
             
@@ -50,18 +67,32 @@ namespace Server
             mNetworkService.Start();
             mNetworkService.SetChannel(true, room,0);
 
+            RequestReady().Wait();
 
+#if DOCKER
+            int minutes = 20;
+            Console.WriteLine("Shutting down the server after {0} minutes.", minutes);
 
+            Task.Delay(minutes * 60 * 1000).Wait();
+
+            mNetworkService.Stop();
+
+            Shutdown().Wait();
+#else
             Console.WriteLine("q: Quit Server.");
             while (true)
             {
                 string input = Console.ReadLine();
-                if(input.Equals("q"))
+                if (input.Equals("q"))
                 {
                     mNetworkService.Stop();
+
+                    Shutdown().Wait();
+
                     break;
                 }
             }
+#endif
 
 
 
@@ -74,5 +105,43 @@ namespace Server
             //	JobTimer.Instance.Flush();
             //}
         }
-	}
+
+        static void InitWebClientService()
+        {
+            mWebClientService.AddHttpClientUri("GameServerManager", $"http://{DockerUtil.GetHostIP()}:7000");
+        }
+
+        //임시
+        static async Task RequestReady()
+        {
+            /// <summary>
+            /// 서버에서 유저를 받아줄 준비를 모두 마쳤다면 호출
+            /// </summary>
+            /// 
+            Console.WriteLine("[RequestReady GameServer]");
+
+            RequestReadyMatchReq request = new RequestReadyMatchReq
+            {
+                container_id = DockerUtil.GetContainerId(),
+            };
+
+            var response = await mWebClientService.PostAsync<RequestReadyMatchRes>("GameServerManager", "Session/RequestReady", request);
+        }
+
+        static async Task Shutdown()
+        {
+            /// <summary>
+            /// 서버에서 유저를 받아줄 준비를 모두 마쳤다면 호출
+            /// </summary>
+            Console.WriteLine("[Shutdown GameServer]");
+
+            ShutdownMatchReq request = new ShutdownMatchReq
+            {
+                container_id = DockerUtil.GetContainerId(),
+            };
+
+            var response = await mWebClientService.PostAsync<ShutdownMatchRes>("GameServerManager", "Session/Shutdown", request);
+        }
+
+    }
 }
