7a3f172b2034b7b0b6b2a85b3e411fa6753989fe
sulbos <dlrmswn990716@gmail.com>
2024-08-18 01:48:23 +0900

    HP 버그 수정

--- GSO_Client/Assets/UIManager.cs
+++ GSO_Client/Assets/UIManager.cs
@@ -6,12 +6,7 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public TMP_Text Health;
-    //TO-DO : 로컬 플레이어 생길 시 제거
- //   public Unit LocalUnit => UnitManager.Instance.CurrentPlayer;
-
     public TMP_Text AmmoText;
-    //public Gun Gun => LocalUnit.GetComponentInChildren<Gun>();
-    // Start is called before the first frame update
     void Start()
     {
         
@@ -25,7 +20,7 @@ public class UIManager : MonoBehaviour
         float MaxHP = Managers.Object.MyPlayer.MaxHp;
         float HP = Managers.Object.MyPlayer.Hp;
         //HP
-        // Health.text =  HP+" / "+MaxHP;
+        Health.text =  HP+" / "+MaxHP;
         //GunAmmo
         //AmmoText.text = Gun.getGunStat().name+" :: "+Gun._curAmmo.ToString();
     }
