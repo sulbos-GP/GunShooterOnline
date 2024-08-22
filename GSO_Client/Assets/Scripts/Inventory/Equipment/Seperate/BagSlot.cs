354acf5ae2388fdcc8fc0032ece46e9121966fe4
packtty18 <packtty18@naver.com>
2024-08-22 21:16:40 +0900

    장비칸 작업중
    장비칸에 아이템을 배치할경우, 다시 그리드로 옮길경우, 교체해야하는경우 1인 클라에서 테스트 완료.
    myPlayerController에 장비칸에 있는 아이템오브젝트 변수 추가. 여기서 중심으로 변경하고 다른 스크립트에서 해당 변수 참조할 예정
    
    todo -> 핸들러 작업 + 기존 moveItem패킷 수정

---
+++ GSO_Client/Assets/Scripts/Inventory/Equipment/Seperate/BagSlot.cs
@@ -0,0 +1,22 @@
+using Google.Protobuf.Protocol;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagSlot : EquipSlot
+{
+    private void Awake()
+    {
+        allowedItemType = ItemType.Bag;
+    }
+
+    protected override void ApplyItemEffects(ItemObject item)
+    {
+        base.ApplyItemEffects(item);
+    }
+
+    protected override void RemoveItemEffects(ItemObject item)
+    {
+        base.RemoveItemEffects(item);
+    }
+}
