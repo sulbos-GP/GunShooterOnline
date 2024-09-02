b2d6dce5299d8803492fd2966cb332df82e96838
sulbos <dlrmswn990716@gmail.com>
2024-09-02 16:31:50 +0900

    gunData 더미 데이터 생성

--- GSO_SocketServer/Server/Game/Object/Gun/GunDataManager.cs
+++ GSO_SocketServer/Server/Game/Object/Gun/GunDataManager.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace Server.Game
 {
-    public class GunStat
+    public class GunDataManager
     {
+        public List<GunData> gunDatas = new List<GunData>();
+
+        public static GunDataManager instance = new GunDataManager();
+
+        public void Init()
+        {
+            gunDatas.Add(new GunData(
+               range = 3,
+               fireRate = 2,
+               ammo = 2,
+               accuracy = 5,
+               damage = 10,
+               bulletObj = null,
+               gunType = GunType.Pistol,
+               reloadTime = 3));
+
+        }
         public float range;       // 사거리.
         public float fireRate;    // 연사 속도. 값과 속도가 반비례
         public int ammo;          // 장탄수(보유장탄)
