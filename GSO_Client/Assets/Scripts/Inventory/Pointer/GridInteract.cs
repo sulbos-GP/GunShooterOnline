77f50f5cf665456374d1d77ad960132dbb423ce7
packtty18 <packtty18@naver.com>
2024-09-04 14:02:45 +0900

    Revert " UI수정 및 저장"
    This reverts commit 28df657040effdd931c347b228959413bbd3bed5.

--- GSO_Client/Assets/Scripts/Inventory/Pointer/GridInteract.cs
+++ GSO_Client/Assets/Scripts/Inventory/Pointer/GridInteract.cs
@@ -5,20 +5,12 @@ using UnityEngine.EventSystems;
 
 public class GridInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public bool activeSwitch = false;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (activeSwitch) 
-        { 
-            InventoryController.invenInstance.SelectedGrid = GetComponent<GridObject>(); 
-        }
-        
+        InventoryController.invenInstance.SelectedGrid = GetComponent<GridObject>();
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (activeSwitch)
-        {
-            InventoryController.invenInstance.SelectedGrid = null;
-        }
+        InventoryController.invenInstance.SelectedGrid = null;
     }
 }
