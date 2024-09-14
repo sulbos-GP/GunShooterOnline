b83da7340dc819992dfd3b11d78a8b5ff7c3b5c1
apeirogon99 <apeirogon99@gmail.com>
2024-09-14 19:33:25 +0900

    유니티 아이콘 이름 변경 및 데이터베이스 추출

--- GSO_Database/MasterDB/RewardMasterTable.sql
+++ GSO_Database/MasterDB/RewardMasterTable.sql
@@ -87,14 +87,16 @@ CREATE TABLE IF NOT EXISTS master_reward_level
 	CONSTRAINT FK_master_reward_level_reward_id_master_reward_base_reward_id FOREIGN KEY (`reward_id`) REFERENCES master_reward_base (`reward_id`) ON DELETE CASCADE ON UPDATE RESTRICT
 );
 
+DELETE FROM master_reward_level;
+
 INSERT INTO master_reward_level (reward_id, level, name, icon) VALUES
-(10001, 2, '1000 골드', 'IconS_GoldOre'),
-(10002, 3, '연료 2개', 'IconR_FuelTank'),
+(10001, 2, '1000 골드', 'IconS_goldore'),
+(10002, 3, '연료 2개', 'IconR_fueltank'),
 (10003, 4, '건전지 5개', 'IconS_battery'),
-(10004, 5, '2000 골드', 'IconS_GoldOre'),
-(10005, 6, '연료 4개', 'IconR_FuelTank'),
+(10004, 5, '2000 골드', 'IconS_goldore'),
+(10005, 6, '연료 4개', 'IconR_fueltank'),
 (10006, 7, '건전지 10개', 'IconS_battery'),
-(10007, 8, '3000 골드', 'IconS_GoldOre'),
-(10008, 9, '연료 6개', 'IconR_FuelTank'),
+(10007, 8, '3000 골드', 'IconS_goldore'),
+(10008, 9, '연료 6개', 'IconR_fueltank'),
 (10009, 10, '건전지 15개', 'IconS_battery');
 
