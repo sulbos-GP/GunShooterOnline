73c36be2185bf2973c598f8f28788e3458fe14d7
sulbos <dlrmswn990716@gmail.com>
2024-09-28 16:03:44 +0900

    엑셀 데이터 구조 수정

--- GSO_Client/Assets/test 1/Data_Master.cs
+++ GSO_Client/Assets/test 1/Data_Master.cs
@@ -52,7 +52,7 @@ public class Data_master_reward_base : BaseData<Data_master_reward_base>
     public int gacha;
     public int experience;
     //TO-DO : 추후에 타입 변경 가능성 있음.
-    public int? reward_box_id;
+    public int reward_box_id;
 }
 
 public class Data_master_reward_level : BaseData<Data_master_reward_level>
@@ -62,7 +62,7 @@ public class Data_master_reward_level : BaseData<Data_master_reward_level>
     public string icon;
 }
 
-public class Data_master_reward_box : BaseData<Data_master_reward_box>
+public class Data_master_reward_box : BaseDataMulti<Data_master_reward_box>
 {
     public int box_scale_x;
     public int box_scale_y;
