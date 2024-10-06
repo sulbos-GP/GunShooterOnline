566ca27ff778b2844a358b9355c20751bc12e53f
apeirogon99 <apeirogon99@gmail.com>
2024-10-06 19:40:50 +0900

    웹 서버

--- GSO_WebServer/WebCommonLibrary/DTO/Performance/PlayerStats.cs
+++ GSO_WebServer/WebCommonLibrary/DTO/Performance/PlayerStats.cs
@@ -6,6 +6,7 @@ namespace WebCommonLibrary.DTO.Performance
     public class PlayerStatsReq
     {
         public string room_token { get; set; } = string.Empty;
+        public int uid { get; set; } = 0;
         public MatchOutcome? outcome { get; set; } = null;
     }
 
