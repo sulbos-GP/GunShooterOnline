902477e0f832782e64cf139f39f1a35f0974f15f
apeirogon99 <apeirogon99@gmail.com>
2024-10-08 21:38:06 +0900

    메타데이터 갱신 추가

---
+++ GSO_WebServer/WebCommonLibrary/DTO/User/Metadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCommonLibrary.Models.GameDatabase;
+using WebCommonLibrary.Models.GameDB;
+
+namespace WebCommonLibrary.DTO.User
+{
+    public class LoadMetadataReq
+    {
+
+    }
+
+    public class LoadMetadataRes : ErrorCodeDTO
+    {
+        public UserMetadataInfo? metadata { get; set; } = null;
+    }
+}
