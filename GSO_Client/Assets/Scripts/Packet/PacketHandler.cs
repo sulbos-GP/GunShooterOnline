064837a86e8b010891d79b3126ad4e93b4855121
sulbos <dlrmswn990716@gmail.com>
2024-10-08 22:38:16 +0900

    버그 수정

--- GSO_Client/Assets/Scripts/Packet/PacketHandler.cs
+++ GSO_Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -113,7 +113,7 @@ internal class PacketHandler
 
         //타 플레이어의 움직임을 조정
 
-        var cc = go.GetComponent<BaseController>();
+        var cc = go.GetComponent<CreatureController>();
         if (cc == null)
             return;
 
