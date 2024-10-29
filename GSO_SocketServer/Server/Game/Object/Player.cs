ccbc6963c04eefaffcb796c763ff166fd6e417fb
jish0902 <84966162+jish0902@users.noreply.github.com>
2024-10-29 21:52:37 +0900

    /
    /

--- GSO_SocketServer/Server/Game/Object/Player.cs
+++ GSO_SocketServer/Server/Game/Object/Player.cs
@@ -132,6 +132,8 @@ public class Player : CreatureObj
             };
             Session.Send(packet);
 
+            Console.WriteLine("Fail to Use" + sourceObjectId);
+
             return;
         }
         
