2a6a1c661ec038cba92d8b22f5bfab77948d30ef
sulbos <dlrmswn990716@gmail.com>
2024-11-12 01:30:05 +0900

    총구 파티클 버그 수정

--- GSO_Client/Assets/test 1/testGunFire.cs
+++ GSO_Client/Assets/test 1/testGunFire.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class testGunFire : MonoBehaviour
 {
     public float time = 0f;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
+        animator = GetComponent<Animator>();
         time += Time.deltaTime;
         if(time>1.0f)
         {
             time -= 1.0f;
+            animator.SetTrigger("Fire");
            //gameObject.GetComponentInChildren<Gun>().Fire();
         }
     }
