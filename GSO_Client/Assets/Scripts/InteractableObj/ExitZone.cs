bdc238aa415ccc90f2bff5b8c9ba97140635e153
apeirogon99 <apeirogon99@gmail.com>
2024-11-23 20:20:00 +0900

    exit zone 테스트 끝

--- GSO_Client/Assets/Scripts/InteractableObj/ExitZone.cs
+++ GSO_Client/Assets/Scripts/InteractableObj/ExitZone.cs
@@ -9,49 +9,31 @@ public class ExitZone : InteractableObject
 
     public float exitTime;  //나가는데 걸리는 시간
     private float remainingTime; // 남은 시간
-    private bool isExiting; // 현재 탈출 중인지 여부
-
-    private float hpIndex;
-    private Vector2 posIndex;
+    private bool isExiting = false; // 현재 탈출 중인지 여부
 
     private Coroutine exitCoroutine;
+    private Coroutine retryCoroutine;
+
     public GameObject timerTextPref; //생성할 타이머의 프리팹
     private GameObject timerUI; //프리팹으로 생성한 UI객체
     private TextMeshProUGUI timeText; //타이머 객체의 텍스트UI
 
     private void Awake()
     {
+        isExiting = false;
         Init();
         SetTriggerSize();
-    }
-
-    private void CreateTimerText()
-    {
-        timerUI = Managers.Resource.Instantiate("UI/TimerText", GameObject.Find("Canvas").transform);
-        timeText= timerUI.GetComponentInChildren<TextMeshProUGUI>();
-        if (timeText != null) {
-            Debug.Log("찾지못함");
-        }
-    }
-
-    private void DestroyTimerText()
-    {
-        timeText = null;
-        Managers.Resource.Destroy(timerUI);
     }
 
     protected override void Init()
     {
         base.Init();
-        isExiting = false;
         interactRange = 1;
         if (exitTime == 0)
         {
             exitTime = 8;
         }
         remainingTime = exitTime;
-        hpIndex = 0;
-        posIndex = Vector2.zero;
     }
 
 
@@ -64,109 +46,93 @@ public class ExitZone : InteractableObject
     [ContextMenu("ExitZone interact")]
     public override void Interact()
     {
-        Debug.Log("interact");
-        if (!isExiting) {
-            CreateTimerText();
-            exitCoroutine = StartCoroutine(ExitCoroutine(exitTime));
-        }
-        
+        if (false == isExiting) 
+        {
+            Debug.Log("ExitZone interact");
 
+            timerUI = Managers.Resource.Instantiate("UI/TimerText", GameObject.Find("Canvas").transform);
+            timeText = timerUI.GetComponentInChildren<TextMeshProUGUI>();
+            if (timeText == null)
+            {
+                Debug.Log("Can't find UI/TimerText");
+                return;
+            }
+            exitCoroutine = StartCoroutine(ExitCoroutine(exitTime));
+
+            C_ExitGame packet = new C_ExitGame()
+            {
+                PlayerId = Managers.Object.MyPlayer.Id,
+                ExitId = objectId
+            };
+            Managers.Network.Send(packet);
+        }
+        else
+        {
+            Debug.Log("already exitZone interact");
+        }
     }
+
     [ContextMenu("ExitZone Cancel")]
-    public void CancelExit()
+    public void CancelExit(float retryTime)
     {
         //피격되거나 움직일 경우 isExiting을 false로 변환할것
         if (exitCoroutine != null)
         {
-            Debug.Log("interrupted");
+            Debug.Log("ExitZone Cancel");
             StopCoroutine(exitCoroutine); // 코루틴 중지
-            DestroyTimerText();
+
+            timeText = null;
+            Managers.Resource.Destroy(timerUI);
+
             Init();
         }
+
+        if(retryCoroutine == null)
+        {
+            retryCoroutine = StartCoroutine(RetryCoroutine(retryTime));
+        }
+    }
+
+    private IEnumerator RetryCoroutine(float retryTime)
+    {
+        Debug.Log("Start RetryCoroutine");
+
+        while (retryTime > 0)
+        {
+            retryTime -= 0.1f;
+            yield return new WaitForSeconds(0.1f); // 1초마다 업데이트
+        }
+
+        Debug.Log("End RetryCoroutine");
+
+        isExiting = false;
+        retryCoroutine = null;
     }
 
     private IEnumerator ExitCoroutine(float exitTime)
     {
-        Debug.Log("Exit");
+        Debug.Log("ExitCoroutine");
 
         isExiting = true;
         remainingTime = exitTime;
-        hpIndex = Managers.Object.MyPlayer.Hp;
-        posIndex = Managers.Object.MyPlayer.transform.position;
         
         while (remainingTime > 0)
         {
-            if (ExitCheck() == false)
+            remainingTime -= 0.1f;
+
+            int seconds = Mathf.FloorToInt(remainingTime);
+            int milliseconds = Mathf.FloorToInt((remainingTime - seconds) * 10);
+
+            Vector2 playerPos = Camera.main.WorldToScreenPoint(Managers.Object.MyPlayer.transform.position);
+            timerUI.transform.position = playerPos + new Vector2(0, 40);
+
+            if (timeText != null)
             {
-                CancelExit();
+                timeText.text = string.Format("{0}:{1}", seconds, milliseconds);
             }
 
-            remainingTime -= 0.1f;
-            UpdateTimerUI(remainingTime); // UI 업데이트
             yield return new WaitForSeconds(0.1f); // 1초마다 업데이트
         }
-        DestroyTimerText();
-        // 탈출 성공 시 처리
-        C_ExitGame packet = new C_ExitGame()
-        {
-            PlayerId = Managers.Object.MyPlayer.Id,
-            ExitId = objectId
-        };
-        Managers.Network.Send(packet);
-
-        Debug.Log("C_LeaveGame");
-
-        //클라이언트의 모든 오브젝트의 내용 클리어
-        Managers.Object.Clear();
-        Managers.Object.DebugDics();
-
-        // 게임 씬을 로비로
-        var evniorment = Managers.EnvConfig.GetEnvironmentConfig();
-        if(evniorment.LobbyName == "Shelter")
-        {
-            Managers.Scene.LoadScene(Define.Scene.Shelter);
-        }
-        else
-        {
-            Managers.Scene.LoadScene(Define.Scene.Lobby);
-        }
-
-
-        isExiting = false; // 탈출 종료
-    }
-
-    private bool ExitCheck()
-    {
-        //hp에 변화가 생길경우 -> 플레이어가 피격당한 상태가 추가될경우 피격으로 바꿀것
-        if(hpIndex != Managers.Object.MyPlayer.Hp)
-        {
-            return false;
-        }
-        
-        //거리가 달라질경우
-        if (Vector2.Distance(posIndex, Managers.Object.MyPlayer.transform.position) > 0.1f)
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    private void UpdateTimerUI(float time)
-    {
-        
-
-        int seconds = Mathf.FloorToInt(time);
-        int milliseconds = Mathf.FloorToInt((time - seconds) * 10);
-
-        Vector2 playerPos = Camera.main.WorldToScreenPoint(Managers.Object.MyPlayer.transform.position);
-        timerUI.transform.position = playerPos + new Vector2(0,40);
-
-        if (timeText != null)
-        {
-            timeText.text = string.Format("{0}:{1}", seconds, milliseconds);
-        }
-        
     }
 
     protected override void OnTriggerEnter2D(Collider2D collision)
