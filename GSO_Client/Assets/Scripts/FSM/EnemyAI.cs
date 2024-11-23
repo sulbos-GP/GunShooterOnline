6b95fbc6422d3a22c9ab3717c77d8fa19dd8f24a
packtty18 <packtty18@naver.com>
2024-11-23 17:19:50 +0900

    오브젝트 매니저 enemyai추가 및 서버에 fsmState 변경점 주석에 추가

---
+++ GSO_Client/Assets/Scripts/FSM/EnemyAI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : CreatureController
+{
+    protected override void Init()
+    {
+        base.Init();
+    }
+}
