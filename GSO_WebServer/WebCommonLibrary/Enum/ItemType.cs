32cbed2f2e448d7b3035f0ec542af05ba4c3f2e4
apeirogon99 <apeirogon99@gmail.com>
2024-12-09 00:04:35 +0900

    고급 박스 추가

---
+++ GSO_WebServer/WebCommonLibrary/Enum/ItemType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WebCommonLibrary.Enum
+{
+    public enum EItemType
+    {
+        [Description("none")]
+        None,
+
+        [Description("Weapone")]
+        Weapone,
+
+        [Description("Defensive")]
+        Defensive,
+
+        [Description("Bag")]
+        Bag,
+
+        [Description("Recovery")]
+        Recovery,
+
+        [Description("Bullet")]
+        Bullet,
+
+        [Description("Spoil")]
+        Spoil
+    }
+}
