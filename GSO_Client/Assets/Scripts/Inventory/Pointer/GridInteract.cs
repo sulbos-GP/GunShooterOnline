ae73b95a5c7003903c50757043b7c7fbc66f6009
packtty18 <packtty18@naver.com>
2024-12-17 13:48:51 +0900

    들고 있는 아이템이 어느 인벤토리 위에 있는지에 따라 무게 텍스트를 업데이트 하여 결과 무게를 보여주도록 구성
    기존에 플레이어 인벤토리 무게 텍스트만 적용되던 구조를 other 인벤토리의 텍스트에도 적용되도록 로직을 수정

--- GSO_Client/Assets/Scripts/Inventory/Pointer/GridInteract.cs
+++ GSO_Client/Assets/Scripts/Inventory/Pointer/GridInteract.cs
@@ -7,10 +7,60 @@ public class GridInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 {
     public void OnPointerEnter(PointerEventData eventData)
     {
-        InventoryController.Instance.SelectedGrid = GetComponent<GridObject>();
+        InventoryController inven = InventoryController.Instance;
+        inven.SelectedGrid = GetComponent<GridObject>();
+
+        if(inven.SelectedItem == null)
+        {
+            return;
+        }
+
+        if (inven.SelectedItem.parentObjId == inven.SelectedItem.backUpParentId)
+        {
+            if (inven.SelectedItem.parentObjId == 0)
+                inven.playerInvenUI.SetWeightText(inven.playerInvenUI.instantGrid.GridWeight, inven.playerInvenUI.instantGrid.limitWeight);
+            else if (inven.SelectedItem.parentObjId > 7)
+                inven.otherInvenUI.SetWeightText(inven.otherInvenUI.instantGrid.GridWeight, inven.otherInvenUI.instantGrid.limitWeight);
+
+            return;
+        }
+
+        AdjustWeight(inven, inven.SelectedItem.parentObjId, inven.SelectedItem.itemWeight, true);
+        AdjustWeight(inven, inven.SelectedItem.backUpParentId, inven.SelectedItem.itemWeight, false);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        InventoryController.Instance.SelectedGrid = null;
+        InventoryController inven = InventoryController.Instance;
+        inven.SelectedGrid = null;
+
+        if (inven.SelectedItem == null)
+        {
+            return;
+        }
+
+        InventoryController.UpdatePlayerWeight(); //모든 아이템의 무게를 원래대로
+        InventoryController.UpdateOtherWeight();
+    }
+
+    private void AdjustWeight(InventoryController inven, int parentId, double weight, bool isAdding)
+    {
+        double curWeight, resultWeight;
+
+        if (parentId == 0 && inven.playerInvenUI?.instantGrid != null)
+        {
+            curWeight = inven.playerInvenUI.instantGrid.GridWeight;
+            resultWeight = isAdding ? curWeight + weight : curWeight - weight;
+            inven.playerInvenUI.SetWeightText(resultWeight, inven.playerInvenUI.instantGrid.limitWeight);
+        }
+        else if (parentId > 7 && inven.otherInvenUI?.instantGrid != null)
+        {
+            curWeight = inven.otherInvenUI.instantGrid.GridWeight;
+            resultWeight = isAdding ? curWeight + weight : curWeight - weight;
+            inven.otherInvenUI.SetWeightText(resultWeight, inven.otherInvenUI.instantGrid.limitWeight);
+        }
+        else
+        {
+            Debug.Log("장비칸이거나 유효하지 않은 아이디입니다.");
+        }
     }
 }
