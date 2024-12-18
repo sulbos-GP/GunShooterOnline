cd2853e3983289a2b7bc55be818d7c56dc25d1a5
packtty18 <packtty18@naver.com>
2024-12-18 16:56:58 +0900

    기존 forest맵에 하우스 테스트 적용+적의 attackLine렌더러에 매테리얼 등록
    다음 맵에 적용할 투명화스크립트를 맵에 적용. 큰집에 들어가면 지붕과 앞벽이 투명화되는걸 확인가능+ 들어가지 않았는데 시야에 든 부분이 투명하게 보임 = 정상. 나중에 사람이 지나갈수 없는 벽에 블록 레이어 추가시 정상작동 예정
    
    어떤 이유인지 적의 공격라인을 그리는 라인 렌더러의 매테리얼이 None으로 되어있어서 공격범위가 안보였음. 디폴트 라인 메테리얼로 등록해놓음

--- GSO_Client/Assets/Scripts/House/HideCeilingByShader.cs
+++ GSO_Client/Assets/Scripts/House/HideCeilingByShader.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.U2D;
+
 
 public class HideCeilingByShader : MonoBehaviour
 {
@@ -19,6 +22,54 @@ public class HideCeilingByShader : MonoBehaviour
     private float targetAlpha = 1f;
     private bool isFading = false;
 
+    private void Update()
+    {
+        if (isFading)
+        {
+            ChangeAlpha(targetAlpha);
+        }
+    }
+
+    private void ChangeAlpha(float targetAlpha)
+    {
+        for (int i = 0; i < targetObjs.Length; i++)
+        {
+            Material transparentMaterial = GetMaterial(targetObjs[i]);
+
+            if (transparentMaterial == null)
+            {
+                Debug.LogError($"등록된 객체 {targetObjs[i]}의 매테리얼을 구하지 못함");
+                isFading = false;
+                return;
+            }
+
+            Color wallColor = transparentMaterial.color;
+            wallColor.a = Mathf.Lerp(wallColor.a, targetAlpha, Time.deltaTime * fadeSpeed);
+            transparentMaterial.color = wallColor;
+
+            if(i == targetObjs.Length - 1 && Mathf.Abs(wallColor.a - targetAlpha) < 0.01f)
+            {
+                isFading = false; // 페이드 완료
+            }
+        }
+    }
+
+    private Material GetMaterial(GameObject target)
+    {
+        if (target.TryGetComponent<SpriteRenderer>(out SpriteRenderer sprite))
+        {
+            return sprite.material;
+        }
+        else if (target.TryGetComponent<TilemapRenderer>(out TilemapRenderer tile))
+        {
+            return tile.material;
+        } //가능성있는 렌더러를 추가
+        else
+        {
+            return null;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -35,33 +86,5 @@ public class HideCeilingByShader : MonoBehaviour
             targetAlpha = onAlpha;
             isFading = true;
         }
-    }
-
-    private void Update()
-    {
-        if (isFading)
-        {
-            ChangeAlpha(targetAlpha);
-        }
-    }
-
-    private void ChangeAlpha(float targetAlpha)
-    {
-        for (int i = 0; i < targetObjs.Length; i++)
-        {
-            SpriteRenderer objectRenderer = targetObjs[i].GetComponent<SpriteRenderer>();
-            Material transparentMaterial = objectRenderer.material;
-
-            Color wallColor = transparentMaterial.color;
-            wallColor.a = Mathf.Lerp(wallColor.a, targetAlpha, Time.deltaTime * fadeSpeed);
-            transparentMaterial.color = wallColor;
-
-            if(i == targetObjs.Length - 1 && Mathf.Abs(wallColor.a - targetAlpha) < 0.01f)
-            {
-                isFading = false; // 페이드 완료
-            }
-        }
-        
-        
     }
 }
